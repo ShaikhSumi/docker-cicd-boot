63cdce099cda1d899910f1647575581b7d6d34e7
ShaikhSumi <suimishaikh@gmail.com>
2026-02-28 00:19:48 +0530

    first commit

---
+++ src/test/java/com/exponent/se/CicdDockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exponent.se;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDockerImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
